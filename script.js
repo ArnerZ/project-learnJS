cd53b9de09dbc6bd7d69f918dc2d06a7585d0899
Stas <example@gmail.com>
2022-09-12 14:08:13 +0300

    обьем куба и площадь

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    // Пишем решение вот тут
-    for(let i = 0; i < data.length; i++){
-        if(typeof(data[i]) === 'number'){
-            data[i] = data[i] * 2;
-        } else if(typeof(data[i]) === 'string') {
-            data[i] = data[i] +  ' done';
-        }
-        
-    }
-
-    console.log(data);
-    return data;
+function calculateVolumeAndArea(side) {
+    if(side != 'string' && side > 0){
+    let a = side * side * side;
+    let b = 6 * side * side;
+    console.log(`обьем куба: ${a}, площадь всей поверхности: ${b}`);
 }
-secondTask();
-
+    else{
+        console.log('писяо');
+    }
+}
+calculateVolumeAndArea('голова');
